f47b518c1b350c78dcc93435465a54e6c500d7c1
Aske Færch Christensen <askefc@gmail.com>
2023-07-04 14:27:17 +0200

    Update
    Use strict javascript.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -7,7 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const port = 4444;
-
 
 app.get('/', (req, res) => {
     res.sendFile('public/index.html', { root: __dirname });
